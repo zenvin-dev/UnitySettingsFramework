12d2a5d2e626c0f7daa0e53c4c053198c4ee25dc
Zenvin <zenvin@outlook.de>
2022-08-13 00:09:16 +0200

    - Try fix `ReferenceFixUtility.FixAsset`

--- Editor/ReferenceFixUtility.cs
+++ Editor/ReferenceFixUtility.cs
@@ -20,6 +20,7 @@ namespace Zenvin.Settings.Framework {
 		private static void FixAsset (SerializedObject obj, SettingsAsset asset) {
 			Log ("<b>SettingsAsset verification log</b>", 0);
 			Log ($"Start verifying asset '{asset.name}'...", 0, 1);
+
 			if (Application.isPlaying) {
 				Log ("Error: Application is in play mode.", 1);
 				return;
@@ -31,16 +32,36 @@ namespace Zenvin.Settings.Framework {
 				return;
 			}
 
-			var allAssets = AssetDatabase.LoadAllAssetsAtPath (path);
-			Log ($"Found {allAssets.Length - 1} sub-assets.", 1);
-
 			List<SettingBase> settings = new List<SettingBase> ();
 			List<SettingsGroup> groups = new List<SettingsGroup> ();
 
+			Object[] allAssets = AssetDatabase.LoadAllAssetsAtPath (path);
+			Log ($"Found {allAssets.Length - 1} sub-assets.", 1);
+
+
 			Log ("Pass 1: Delete assets with missing/invalid types...", 1, 1);
-			int deleted = 0;
+			ClearInvalidObjects (allAssets, groups, settings, out int deleted, out int cleared);
+			Log ($"Deleted {deleted} assets and cleared {cleared} null references.", 2);
+
+			Log ($"Evaluation found {groups.Count} total groups and {settings.Count} total settings.", 1, 1);
+
+			Log ("Pass 2: Restore references...", 1, 1);
+			int groupsRestored = RestoreGroupReferences (asset, groups);
+			int settingsRestored = RestoreSettingReferences (asset, groups, settings);
+			Log ($"Restored {groupsRestored} group references and {settingsRestored} setting references.", 2);
+
+			obj.ApplyModifiedProperties ();
+			AssetDatabase.Refresh ();
+			AssetDatabase.SaveAssets ();
+			Log ("Asset integrity verified.", 0, 1);
+		}
+
+		private static void ClearInvalidObjects (Object[] allAssets, List<SettingsGroup> groups, List<SettingBase> settings, out int deleted, out int cleared) {
+			deleted = 0;
+			cleared = 0;
 			foreach (var subAsset in allAssets) {
-				if (subAsset is SettingsAsset) {
+				if (subAsset is SettingsAsset sa) {
+					ClearNullReferences (sa, ref cleared);
 					continue;
 				}
 
@@ -53,6 +74,7 @@ namespace Zenvin.Settings.Framework {
 
 				switch (subAsset) {
 					case SettingsGroup group:
+						ClearNullReferences (group, ref cleared);
 						groups.Add (group);
 						break;
 					case SettingBase setting:
@@ -60,94 +82,84 @@ namespace Zenvin.Settings.Framework {
 						break;
 				}
 			}
-			Log ($"Deleted {deleted} assets.", 2);
-
-			Log ("Pass 2: Restore references...", 1, 1);
-			int restored = 0;
-			for (int i = 0; i < settings.Count; i++) {
-				SettingBase set = settings[i];
-
-				if (set == null) {
-					continue;
-				}
-				if (set.asset != null && set.group != null) {
-					continue;
-				}
-				if (set.group == null) {
-					SettingsGroup parentGroup = asset;
-					foreach (var grp in groups) {
-						if (grp != null && grp.Settings.Contains (set)) {
-							parentGroup = grp;
-							break;
-						}
-					}
-					set.group = parentGroup;
-					if (!parentGroup.settings.Contains (set)) {
-						parentGroup.settings.Add (set);
-					}
-					restored++;
-					Log ($"Established setting {set} as child of group {parentGroup}.", 2);
-				}
-				if (set.asset == null) {
-					set.asset = asset;
-					restored++;
-					Log ($"Restored asset reference in setting {set}.", 2);
-				}
-			}
-			for (int i = 0; i < groups.Count; i++) {
-				SettingsGroup group = groups[i];
-
-				if (group == null) {
-					continue;
-				}
-				if (group.Parent == null) {
-					SettingsGroup parentGroup = asset;
-					foreach (var grp in groups) {
-						if (grp != group && grp.Groups.Contains (group)) {
-							parentGroup = grp;
-							break;
-						}
-					}
-
-					group.Parent = parentGroup;
-					if (!parentGroup.groups.Contains (group)) {
-						parentGroup.groups.Add (group);
-					}
-					restored++;
-					Log ($"Re-established group {group} as child of group {parentGroup}.", 2);
-				}
-
-			}
-			Log ($"Restored {restored} references.", 2);
-
-
-			Log ("Pass 3: Clear null references...", 1, 1);
-			groups.Add (asset);
-			int cleaned = 0;
-			for (int i = groups.Count - 1; i >= 0; i--) {
-				if (groups[i] == null) {
-					groups.RemoveAt (i);
-					i--;
-					cleaned++;
-					continue;
-				}
-
-				for (int j = groups[i].settings.Count - 1; j >= 0; j--) {
-					if (groups[i].settings[j] == null) {
-						groups[i].settings.RemoveAt (j);
-						j--;
-						cleaned++;
-					}
-				}
-			}
-			Log ($"Cleared {cleaned} null references.", 2);
-
-			obj.ApplyModifiedProperties ();
-			AssetDatabase.Refresh ();
-			AssetDatabase.SaveAssets ();
-			Log ("Asset integrity verified.", 0, 1);
 		}
 
+		private static void ClearNullReferences (SettingsGroup group, ref int cleared) {
+			for (int i = group.groups.Count - 1; i >= 0; i--) {
+				if (group.groups[i] == null) {
+					group.groups.RemoveAt (i);
+					i--;
+					cleared++;
+				}
+			}
+			for (int i = group.settings.Count - 1; i >= 0; i--) {
+				if (group.settings[i] == null) {
+					group.settings.RemoveAt (i);
+					i--;
+					cleared++;
+				}
+			}
+		}
+
+		private static int RestoreGroupReferences (SettingsAsset asset, List<SettingsGroup> groups) {
+			int referenced = 0;
+
+			foreach (var group in groups) {
+				if (group.Parent != null) {
+					continue;
+				}
+
+				SettingsGroup parentGroup = asset;
+				foreach (var _group in groups) {
+					if (_group != group && _group.groups.Contains (group)) {
+						parentGroup = _group;
+						break;
+					}
+				}
+
+				group.Parent = parentGroup;
+				if (parentGroup == asset && !parentGroup.groups.Contains (group)) {
+					parentGroup.groups.Add (group);
+				}
+
+				referenced++;
+				Log ($"Established group {group} as child of group {parentGroup}.", 2);
+			}
+
+			return referenced;
+		}
+
+		private static int RestoreSettingReferences (SettingsAsset asset, List<SettingsGroup> groups, List<SettingBase> settings) {
+			int referenced = 0;
+
+			foreach (var setting in settings) {
+				if (setting.asset == null) {
+					setting.asset = asset;
+				}
+
+				if (setting.group != null) {
+					continue;
+				}
+
+				SettingsGroup parentGroup = asset;
+				foreach (var group in groups) {
+					if (group.settings.Contains (setting)) {
+						parentGroup = group;
+						break;
+					}
+				}
+
+				setting.group = parentGroup;
+				if (parentGroup == asset && !parentGroup.settings.Contains (setting)) {
+					parentGroup.settings.Add (setting);
+				}
+
+				referenced++;
+				Log ($"Established setting {setting} as child of group {parentGroup}.", 2);
+			}
+
+			return referenced;
+		}
 
 		private static void Log (string message, int indent, int lineBreaks = 0) {
 			sb.AppendLine (new string ('\n', lineBreaks) + new string (' ', indent * 4) + message);
