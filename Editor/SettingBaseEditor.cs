f61757133b8b8b3c2715777ad35162a26800593e
Zenvin <zenvin@outlook.de>
2023-10-28 18:18:09 +0200

    - Disable `SettingBaseEditor` for debug purposes

--- Editor/SettingBaseEditor.cs
+++ Editor/SettingBaseEditor.cs
@@ -3,8 +3,8 @@ using UnityEditor;
 namespace Zenvin.Settings.Framework {
 	[CustomEditor (typeof (SettingBase), true, isFallback = true)]
 	public class SettingBaseEditor : Editor {
-		public override void OnInspectorGUI () {
+		//public override void OnInspectorGUI () {
 			
-		}
+		//}
 	}
 }
