8f0e3168b6a3d2800c0b3103b1cf4002d97cead3
Zenvin <zenvin@outlook.de>
2024-01-17 18:51:28 +0100

    - Remove unused class `SettingData`

--- Runtime/Framework/Serialization/DataModels.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Assets.UnitySettingsFramework.Runtime.Framework.Serialization {
-	[Serializable]
-	internal class SettingData {
-		public string GUID { get; set; }
-		public byte[] Data { get; set; }
-	}
-}
